0464f42d82b462334e9ac1520d26e06d0eec9ec1
Beth Ann Shadwick <beth.shadwick@smail.astate.edu>
2020-04-07 11:27:00 -0500

    A few changes

--- main.js
+++ main.js
@@ -11,22 +11,22 @@ function randomValueFromArray(array){
 
 2. RAW TEXT STRINGS
 
-let storyText='It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
+let storyText=['It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.'];
 
 let insertX=
-['Willy the Goblin
-Big Daddy
-Father Christmas'];
+['Willy the Goblin',
+'Big Daddy',
+'Father Christmas'];
 
 let insertY=
-['the soup kitchen
-Disneyland
-the White House'];
+['the soup kitchen',
+'Disneyland',
+'the White House'];
 
 let insertZ=
-['spontaneously combusted
-melted into a puddle on the sidewalk
-turned into a slug and crawled away'];
+['spontaneously combusted',
+'melted into a puddle on the sidewalk',
+'turned into a slug and crawled away'];
 
 3. EVENT LISTENER AND PARTIAL FUNCTION DEFINITION
 
